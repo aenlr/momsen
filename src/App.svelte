e551a51728738fbffef96c763791045eed871364
Anders Norlander <33195524+aenlr@users.noreply.github.com>
2025-06-06 23:27:05 +0200

    Ikoner, metadata och persistens av inmatning.

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,39 @@
 <script>
   import {Pris} from "./lib/Pris.svelte.js";
   import Toggle from "./lib/Toggle.svelte";
+  import {onMount} from "svelte";
 
   const pris = new Pris()
-  let priskalkyl = false
+  let priskalkyl = $state(false)
+  let initialized = false
+
+  $effect(() => {
+    const state = {
+      pris,
+      priskalkyl
+    }
+    const json = JSON.stringify(state)
+    if (initialized) {
+      sessionStorage.setItem('state', json)
+    }
+  })
+
+  onMount(() => {
+    initialized = true
+    const json = sessionStorage.getItem('state')
+    if (!json) {
+      return
+    }
+    try {
+      const state = JSON.parse(json)
+      priskalkyl = state.priskalkyl ?? priskalkyl
+      if (state.pris) {
+        pris.fromJSON(state.pris)
+      }
+    } catch (e) {
+      console.error('State initialization error')
+    }
+  })
 
   function onclick(event) {
     event.target.select()
@@ -18,13 +48,11 @@
 
 {#snippet Momssats()}
   {@render Label("moms", "Momssats")}
-  <div class="border border-gray-200 rounded p-2 focus-within:outline-blue-500 focus-within:outline-2">
-    <select id="moms" bind:value={pris.moms} class="w-full outline-none sm:text-lg">
-      <option value={12}>12% (livsmedel)</option>
-      <option value={25}>25% (vanligast)</option>
-      <option value={6}>6%</option>
-    </select>
-  </div>
+  <select id="moms" bind:value={pris.moms} class="w-full sm:text-lg border border-gray-200 rounded px-4 py-2 focus:outline-blue-500 focus:outline-2">
+    <option value={12}>12% (livsmedel)</option>
+    <option value={25}>25% (vanligast)</option>
+    <option value={6}>6%</option>
+  </select>
 {/snippet}
 
 {#snippet InklusiveMoms()}
@@ -40,8 +68,8 @@
          onfocus={() => pris.fokus = 'exkl'} onblur={pris.exkl.commit}>
 {/snippet}
 
-<div class="max-w-xl mx-auto sm:mt-10">
-  <main class="p-4">
+<div class="max-w-xl mx-auto sm:mt-10 p-4">
+  <main class="">
     <div class="flex items-center">
       <h1 class="font-medium text-3xl text-center mb-2 mr-auto">Momsen</h1>
       <Toggle id="kalkylator" enabled={priskalkyl} onclick={() => priskalkyl = !priskalkyl} label="Priskalkyl"/>
@@ -70,4 +98,19 @@
       {/if}
     </div>
   </main>
+
+  <footer class="[@media(min-height:568px)]:fixed bottom-0 py-4">
+    <p class="text-xs">
+      Ett verktyg från <a href="https://mitang.se" class="underline">Mitang AB</a>
+    </p>
+  </footer>
 </div>
+
+<style>
+  select {
+    background: url(data:image/svg+xml;base64,PHN2ZyBpZD0iTGF5ZXJfMSIgZGF0YS1uYW1lPSJMYXllciAxIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCA0Ljk1IDEwIj48ZGVmcz48c3R5bGU+LmNscy0xe2ZpbGw6I2ZmZjt9LmNscy0ye2ZpbGw6IzQ0NDt9PC9zdHlsZT48L2RlZnM+PHRpdGxlPmFycm93czwvdGl0bGU+PHJlY3QgY2xhc3M9ImNscy0xIiB3aWR0aD0iNC45NSIgaGVpZ2h0PSIxMCIvPjxwb2x5Z29uIGNsYXNzPSJjbHMtMiIgcG9pbnRzPSIxLjQxIDQuNjcgMi40OCAzLjE4IDMuNTQgNC42NyAxLjQxIDQuNjciLz48cG9seWdvbiBjbGFzcz0iY2xzLTIiIHBvaW50cz0iMy41NCA1LjMzIDIuNDggNi44MiAxLjQxIDUuMzMgMy41NCA1LjMzIi8+PC9zdmc+) no-repeat 95% 50%;
+    -moz-appearance: none;
+    -webkit-appearance: none;
+    appearance: none;
+  }
+</style>
--- src/lib/Pris.svelte.js
+++ src/lib/Pris.svelte.js
@@ -1,174 +1,237 @@
 let separator = $state(1.5.toLocaleString().includes(',') ? ',' : '.')
 
 class Pristext {
-    #text = $state('')
-    #prev = $state('')
-    #value = $state(0)
-    #update
-    #valid = true
-    #decimaler = 0
+  #text = $state('')
+  #prev = $state('')
+  #value = $state(0)
+  #update
+  #valid = true
+  #decimaler = 0
 
-    constructor(value, decimaler, update) {
-        this.#value = value
-        this.#decimaler = decimaler
-        this.#text = value.toFixed(decimaler).replace('.', separator)
-        this.#prev = this.#text
-        this.#update = update
+  constructor(value, decimaler, update) {
+    this.#value = value
+    this.#decimaler = decimaler
+    this.#text = value.toFixed(decimaler).replace('.', separator)
+    this.#prev = this.#text
+    this.#update = update
+  }
+
+  get valid() {
+    return this.#valid
+  }
+
+  get value() {
+    return this.#value
+  }
+
+  set value(value) {
+    if (value !== this.#value) {
+      this.#valid = true
+      this.#value = value
+      this.commit()
+    } else if (!this.#valid) {
+      this.#valid = true
+      this.#text = this.#prev
+    }
+  }
+
+  get text() {
+    return this.#text
+  }
+
+  set text(value) {
+    if (value === this.#text) {
+      return
     }
 
-    get valid() {
-        return this.#valid
+    const prev = this.#text
+    this.#text = value
+    let n
+    if (typeof value === 'string') {
+      n = parseFloat(value.replace(',', '.'))
+    } else if (typeof value === 'number') {
+      n = value
+    } else {
+      this.#valid = false
+      return
     }
 
-    get value() {
-        return this.#value
+    if (isNaN(n)) {
+      this.#valid = false
+      return
     }
 
-    set value(value) {
-        if (value !== this.#value) {
-            this.#valid = true
-            this.#value = value
-            this.commit()
-        } else if (!this.#valid) {
-            this.#valid = true
-            this.#text = this.#prev
-        }
+    if (this.#valid) {
+      this.#prev = prev
+    }
+    this.#valid = true
+    this.#value = n
+    this.#update(n)
+  }
+
+  commit = () => {
+    if (this.#valid) {
+      this.#text = this.#value.toFixed(this.#decimaler).replace('.', separator)
+    } else {
+      this.#valid = true
+      this.#text = this.#prev
+    }
+  }
+
+  toJSON() {
+    if (this.#valid) {
+      return this.value
     }
 
-    get text() {
-        return this.#text
+    return {
+      value: this.#value,
+      text: this.#text,
+      prev: this.#prev
     }
+  }
 
-    set text(value) {
-        if (value === this.#text) {
-            return
-        }
-
-        const prev = this.#text
-        this.#text = value
-        let n
-        if (typeof value === 'string') {
-            n = parseFloat(value.replace(',', '.'))
-        } else if (typeof value === 'number') {
-            n = value
-        } else {
-            this.#valid = false
-            return
-        }
-
-        if (isNaN(n)) {
-            this.#valid = false
-            return
-        }
-
+  fromJSON(json) {
+    if (typeof json === 'number') {
+      this.#value = json
+      this.#valid = true
+      this.commit()
+      this.#prev = this.#text
+    } else if (typeof json === 'string') {
+      this.#text = json
+      try {
+        const n = parseFloat(json.replace(',', '.'))
+        this.#valid = !isNaN(n)
         if (this.#valid) {
-            this.#prev = prev
+          this.#value = n
+          this.#prev = json
         }
-        this.#valid = true
-        this.#value = n
-        this.#update(n)
+      } catch (e) {
+        this.#valid = false
+      }
+    } else if (json && typeof json === 'object') {
+      this.#value = json.value ?? this.#value
+      this.#text = json.text ?? this.#text
+      this.#prev = json.prev ?? this.#prev
+      try {
+        const n = parseFloat(this.#text.replace(',', '.'))
+        this.#valid = !isNaN(n)
+      } catch (e) {
+        this.#valid = false
+      }
     }
-
-    commit = () => {
-        if (this.#valid) {
-            let t = this.#value.toFixed(this.#decimaler)
-            if (separator !== '.' && this.#text.includes(separator)) {
-                 t = t.replace('.', separator)
-            }
-            this.#text = t
-        } else {
-            this.#text = this.#prev
-        }
-    }
+  }
 }
 
 export class Pris {
-    #inkop = new Pristext(0, 2, () => this.#inkopAndrad())
-    #moms = $state(12)
-    #exkl = new Pristext(0, 2, () => this.#exklAndrad())
-    #inkl = new Pristext(0, 2, () => this.#inklAndrad())
-    #marginal = new Pristext(0, 2, () => this.#marginalAndrad())
-    #vinst = new Pristext(0, 2, () => this.#vinstAndrad())
-    fokus = $state('')
+  #inkop = new Pristext(0, 2, () => this.#inkopAndrad())
+  #moms = $state(12)
+  #exkl = new Pristext(0, 2, () => this.#exklAndrad())
+  #inkl = new Pristext(0, 2, () => this.#inklAndrad())
+  #marginal = new Pristext(0, 2, () => this.#marginalAndrad())
+  #vinst = new Pristext(0, 2, () => this.#vinstAndrad())
+  fokus = $state('')
 
-    _vinst() {
-        return this.#exkl.value - this.#inkop.value
+  _vinst() {
+    return this.#exkl.value - this.#inkop.value
+  }
+
+  _marginal() {
+    return this.#inkop.value > 0 ? 100 * (this.#exkl.value - this.#inkop.value) / this.#inkop.value : 0
+  }
+
+  _inkl() {
+    return this.#exkl.value * (100 + this.#moms) / 100
+  }
+
+  _exkl() {
+    return this.#inkl.value * 100 / (100 + this.#moms)
+  }
+
+  #vinstAndrad() {
+    this.#exkl.value = this.#inkop.value + this.#vinst.value
+    this.#inkl.value = this._inkl()
+    this.#marginal.value = this._marginal()
+  }
+
+  #marginalAndrad() {
+    this.#exkl.value = this.#inkop.value * (100 + this.#marginal.value) / 100
+    this.#inkl.value = this._inkl()
+    this.#vinst.value = this._vinst()
+  }
+
+  #exklAndrad() {
+    this.fokus = 'exkl'
+    this.#vinst.value = this._vinst()
+    this.#inkl.value = this._inkl()
+    this.#marginal.value = this._marginal()
+  }
+
+  #inklAndrad() {
+    this.fokus = 'inkl'
+    this.#exkl.value = this._exkl()
+    this.#vinst.value = this._vinst()
+    this.#marginal.value = this._marginal()
+  }
+
+  #inkopAndrad() {
+    this.#vinst.value = this._vinst()
+    this.#marginal.value = this._marginal()
+  }
+
+  get exkl() {
+    return this.#exkl
+  }
+
+  get inkl() {
+    return this.#inkl
+  }
+
+  get vinst() {
+    return this.#vinst
+  }
+
+  get marginal() {
+    return this.#marginal
+  }
+
+  get moms() {
+    return this.#moms
+  }
+
+  set moms(value) {
+    this.#moms = value
+    if (this.fokus === 'exkl') {
+      this.#inkl.value = this._inkl()
+    } else {
+      this.#exkl.value = this._exkl()
     }
+  }
 
-    _marginal() {
-        return this.#inkop.value > 0 ? 100 * (this.#exkl.value - this.#inkop.value) / this.#inkop.value : 0
+  get inkop() {
+    return this.#inkop
+  }
+
+  toJSON() {
+    return {
+      inkl: this.#inkl,
+      exkl: this.#exkl,
+      moms: this.#moms,
+      inkop: this.#inkop,
+      marginal: this.#marginal,
+      vinst: this.#vinst,
+      fokus: this.fokus
     }
+  }
 
-    _inkl() {
-        return this.#exkl.value * (100 + this.#moms) / 100
+  fromJSON(json) {
+    if (typeof json === 'object') {
+      this.#exkl.fromJSON(json.exkl)
+      this.#inkl.fromJSON(json.inkl)
+      this.#marginal.fromJSON(json.marginal)
+      this.#vinst.fromJSON(json.vinst)
+      this.#inkop.fromJSON(json.inkop)
+      this.#moms = json.moms || this.#moms
+      this.fokus = json.fokus || this.fokus
     }
-
-    _exkl() {
-        return this.#inkl.value * 100 / (100 + this.#moms)
-    }
-
-    #vinstAndrad() {
-        this.#exkl.value = this.#inkop.value + this.#vinst.value
-        this.#inkl.value = this._inkl()
-        this.#marginal.value = this._marginal()
-    }
-
-    #marginalAndrad() {
-        this.#exkl.value = this.#inkop.value * (100 + this.#marginal.value) / 100
-        this.#inkl.value = this._inkl()
-        this.#vinst.value = this._vinst()
-    }
-
-    #exklAndrad() {
-        this.fokus = 'exkl'
-        this.#vinst.value = this._vinst()
-        this.#inkl.value = this._inkl()
-        this.#marginal.value = this._marginal()
-    }
-
-    #inklAndrad() {
-        this.fokus = 'inkl'
-        this.#exkl.value = this._exkl()
-        this.#vinst.value = this._vinst()
-        this.#marginal.value = this._marginal()
-    }
-
-    get exkl() {
-        return this.#exkl
-    }
-
-    get inkl() {
-        return this.#inkl
-    }
-
-    get vinst() {
-        return this.#vinst
-    }
-
-    get marginal() {
-        return this.#marginal
-    }
-
-    get moms() {
-        return this.#moms
-    }
-
-    set moms(value) {
-        this.#moms = value
-        if (this.fokus === 'exkl') {
-            this.#inkl.value = this._inkl()
-        } else {
-            this.#exkl.value = this._exkl()
-        }
-    }
-
-    get inkop() {
-        return this.#inkop
-    }
-
-    #inkopAndrad() {
-        this.#vinst.value = this._vinst()
-        this.#marginal.value = this._marginal()
-    }
-
+  }
 }
